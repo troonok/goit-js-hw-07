cc030379ccaf36328b50094170541392d8a32ce8
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-01-10 04:57:44 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgsGallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+	.map(({original,preview,description}) => `
+    <div class="gallery__item">
+    <a class="gallery__link" href=${original} ><img class="gallery__image" src=${preview} data-source=${original} alt='${description}'></img></a></div>`)
+	.join('');
+imgsGallery.insertAdjacentHTML('afterbegin', markup)
+
+imgsGallery.addEventListener('click',onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const modalImage = basicLightbox.create(`<img src='${e.target.dataset.source}'>`);
+
+  modalImage.show();
+
+  document.addEventListener(
+    'keydown',
+    e => {
+      if (e.code !== 'Escape') {
+        return;
+      }
+      modalImage.close();
+    },
+    { once: true },
+  );
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgsGallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+	.map(({original,preview,description}) => 
+    `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+    </a>`)
+	.join('');
+
+imgsGallery.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+// console.log(galleryItems);
+
